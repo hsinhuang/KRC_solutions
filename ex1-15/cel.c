d2f667d2555d8efcaffcef5e930fd8aa0cd01d73
leon-huang <leon@njuopen.com>
2012-11-05 16:57:31 +0800

    ex1-15

---
+++ ex1-15/cel.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+double f2c(double fahr);
+
+int main(int argc, char const *argv[])
+{
+	int fahr, celsius;
+	for (fahr = LOWER; fahr <= UPPER; fahr += STEP) {
+		printf("%3d %6.1f\n", fahr, f2c(fahr));
+	}
+
+	return 0;
+}
+
+double f2c(double fahr){
+	return (5.0/9.0) * (fahr - 32);
+}
