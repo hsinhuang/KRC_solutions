62eea43dfc735c9de29fd56896e38e5546920feb
leon-huang <leon@njuopen.com>
2012-11-05 21:06:36 +0800

    Fixed a vital bug

--- ex1-16/echoline.c
+++ ex1-16/echoline.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
-#define MAXLINE 10
+#define MAXLINE 1000
 
 // Actually, it is getline in KRC, but when I compiled this file 
 // with gcc 4.6.3, there was an error: conflicting types for ‘getline’
 // so I changed the name of the function to `readline`
 int readline(char line[], int maxline);
-void copy(char to[], char from[]);
 
 int main(int argc, char const *argv[])
 {
 	int len;
 	char line[MAXLINE];
 	while((len = readline(line, MAXLINE)) > 0){
-		while(line[len - 2] != '\n'){
+		while(len == MAXLINE - 1 && line[len - 2] != '\n'){
 			printf("%s", line);
 			len = readline(line, MAXLINE);
 		}
-		printf("%s\n", line);
+		printf("%s", line);
 	}
 	
 	return 0;
@@ -34,11 +33,4 @@ int readline(char s[], int lim){
 	}
 	s[i] = '\0';
 	return i;
-}
-
-void copy(char to[], char from[]){
-	int i = 0;
-
-	while((to[i] = from[i]) != '\0')
-		++i;
 }
